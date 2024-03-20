dec3a84692c8021fbc9fb170313b0f270a4b375a
Andre LaFleur <andre.lafleur@techcyte.com>
2024-03-20 14:57:36 -0600

    Will this work?

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log('>>> HELLO, WORLD!');
+if (!window.gameAndreMade) {
+	window.gameAndreMade = new Phaser.Game({
+		type: Phaser.AUTO,
+		width: 400,
+		height: 400,
+		scene: {
+			preload: () => {},
+			create: () => {},
+			update: () => {},
+		},
+		physics: {},
+	});
+}
